50a68201aa1b6ad864d9c0403dc8df6b04429cc3
karnstem <mackenzierosekarnstein@gmail.com>
2026-03-11 01:55:48 -0700

    Add files via upload

--- FarmToTable.js
+++ FarmToTable.js
@@ -1,5 +1,5 @@
 
-//GEOG 572 Lab 4
+//GEOG 572 FINAL PROJECT
 // Mackenzie Karnstein
 
 //Note: ChatGTP was used to guide code development/editing [including for explaining javascript and Leaflet logistics, editing, and troubleshooting.]
@@ -22,6 +22,50 @@ function add_second_layer(){
 var map;
 var geojson;
 var geojson_2;
+var rad_movableSymbol;
+
+//load icons
+var farm_icon = L.icon({
+    iconUrl: 'FarmMapSymbol.png',
+    iconSize:[16,18.5],
+    iconAnchor:[16,37],
+    popupAnchor:[0,-28]
+
+});
+
+var farm_icon2 = L.icon({
+    iconUrl: 'FarmMapSymbol.png',
+    iconSize:[32,37],
+    iconAnchor:[16,37],
+    popupAnchor:[0,-28]
+
+});
+
+var fish_icon = L.icon({
+    iconUrl: 'FishSymbol.png',
+    iconSize:[16,18.5],
+    iconAnchor:[16,37],
+    popupAnchor:[0,-28]
+
+});
+
+var fish_icon2 = L.icon({
+    iconUrl: 'FishSymbol.png',
+    iconSize:[32,37],
+    iconAnchor:[16,37],
+    popupAnchor:[0,-28]
+
+});
+
+var my_home = L.icon({
+    iconUrl: 'HouseSymbol.png',
+    iconSize:[32,37],
+    iconAnchor:[16,37],
+    popupAnchor:[0,-28]
+
+});
+
+
 
 //define conversion function
 function conversion(response){
@@ -34,15 +78,30 @@ function callback(response2){
 
     console.log(response2);
     //create map element
-    map = L.map('map').setView([44.0, -120.5], 7);
+    map = L.map('map').setView([44.0, -120.5], 6);
     //add tile layer
-    var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    
+    var tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
     }).addTo(map);
+    
+    
+    
+    
+  //  var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //      maxZoom: 19,
+  //      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  //  }).addTo(map);
 
     //Add features to map
-    geojson = L.geoJson(response2).addTo(map);
+    geojson = L.geoJson(response2,{
+        pointToLayer: function(feature,latlng){
+            return L.marker(latlng,{icon:farm_icon})
+        }
+    }).addTo(map);
 
     //start function for loading second layer
     add_second_layer();
@@ -59,8 +118,99 @@ function callback1(response_second2){
     console.log(response_second2);
 
     //Add second set of features to map
-     geojson_2 = L.geoJson(response_second2).addTo(map);
+     geojson_2 = L.geoJson(response_second2,{
+        pointToLayer: function(feature,latlng){
+            return L.marker(latlng,{icon:fish_icon})
+        }
+    }).addTo(map);
 
+    rad_movableSymbol = L.circle([44.0,-120.5],{
+        radius: 80000,
+        fillColor:'#8eb0a3',
+        fillOpacity: 0.3,
+        color:'#8eb0a3'
+    }).addTo(map);
+
+    var movableSymbol = L.marker([44.0, -120.5],{
+        icon: my_home,
+        draggable: true
+    }).addTo(map);
+
+    movableSymbol.on('drag', function(e){
+        rad_movableSymbol.setLatLng(e.target.getLatLng());
+        ModifyIcons();
+    });
+
+    var slide = document.getElementById('radiusSlide');
+
+    slide.addEventListener('input', function(){
+        rad_movableSymbol.setRadius(Number(slide.value));
+        ModifyIcons();
+    });
+
+     extras()
+}
+
+function extras(extras_response){
+    var Combined_Point_Layers = {
+    "Farms": geojson,
+    "Fish": geojson_2
+    };
+    L.control.layers(null, Combined_Point_Layers, { collapsed: false}).addTo(map);
+    
+    ModifyIcons();
+    map.on('overlayadd', ModifyIcons);
+    map.on('overlayremove', ModifyIcons);
+};
+
+function FindRadius(){
+    let location = rad_movableSymbol.getLatLng();
+    return turf.circle(
+        [location.lng, location.lat],
+        rad_movableSymbol.getRadius(),
+        {steps: 64, units: 'meters'}
+    );
+}
+
+function ModifyIcons(){
+    if (!rad_movableSymbol) return;
+    let turf_circle_radius = FindRadius();
+
+    let farmInfo = [];
+    let fishInfo = [];
+
+    geojson.eachLayer(function(layer){
+        let point = turf.point([layer.getLatLng().lng, layer.getLatLng().lat]);
+        if(!map.hasLayer(layer)) return;
+        if (turf.booleanPointInPolygon(point, turf_circle_radius)){
+            layer.setIcon(farm_icon2);
+            farmInfo.push(layer.feature.properties.name);
+        } else {
+            layer.setIcon(farm_icon)
+            };
+    });
+
+
+    geojson_2.eachLayer(function(layer){
+        let point = turf.point([layer.getLatLng().lng, layer.getLatLng().lat]);
+        if(!map.hasLayer(layer)) return;
+        if (turf.booleanPointInPolygon(point, turf_circle_radius)){
+            layer.setIcon(fish_icon2);
+            fishInfo.push(layer.feature.properties.name);
+        } else {
+            layer.setIcon(fish_icon)
+            };
+    });
+
+    let HTML_List = document.getElementById('displayAttributes');
+    HTML_List.innerHTML = '';
+    let all_info = farmInfo.concat(fishInfo);
+    all_info.forEach(name =>{
+        let li = document.createElement('li');
+        li.textContent = name;
+        HTML_List.appendChild(li);
+    });
+    
 }
 window.onload = jsAjax;
 
