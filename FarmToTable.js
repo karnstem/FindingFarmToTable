adab0f2cbd399ea647c21c8157ff8cd25260cf08
karnstem <mackenzierosekarnstein@gmail.com>
2026-03-11 04:10:08 -0700

    Add files via upload

--- FarmToTable.js
+++ FarmToTable.js
@@ -81,11 +81,10 @@ function callback(response2){
     map = L.map('map').setView([44.0, -120.5], 6);
     //add tile layer
     
-    var tiles = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
+    var tiles = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
     }).addTo(map);
     
     
